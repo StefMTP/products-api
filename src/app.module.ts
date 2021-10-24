0af469bd3a808610288a21c764bfb68298720351
Stefanos Michalas <sm@think-plus.gr>
2021-10-24 17:49:13 +0300

    environmental variables for typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,15 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ProductsModule,
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
+    TypeOrmModule.forRoot({
         type: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
         autoLoadEntities: true,
         synchronize: true,
-      })
-      ,
     }),
     ConfigModule.forRoot({
       isGlobal: true,
